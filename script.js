675822f05af9e4899bd2252dfbb9b4ec39ccc788
diyor011 <104572938+diyor011@users.noreply.github.com>
2025-06-17 13:50:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let userName = prompt('Ismingizni kiriting')
+let userid = prompt('Space idingizni kiriting')
+let userpassWord = prompt('Space passwordingizni kiriting')
+
+
+
+
+
+let user = {
+ name: userName,
+ id: userid,
+ password: userpassWord,
+}
+
+console.log(user);
+
